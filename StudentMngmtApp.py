58ae61c22004c4966500c230e11ec8f363669106
abderem <abder.em@gmail.com>
2018-06-24 11:39:09 +0300

    Test 1

--- StudentMngmtApp.py
+++ StudentMngmtApp.py
@@ -4,7 +4,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 def print_students_titlecase():
@@ -39,7 +39,6 @@ student_name = input("Enter student name: ")
 student_id = input("Enter student ID: ")
 
 add_student(student_name, student_id)
-
-read_file()
+save_file(student_name)
 
 
